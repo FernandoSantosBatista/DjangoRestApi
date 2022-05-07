92997e82cc5f165a450df9c128da14ca68495095
FernandodotDev <fernandodossantos005@gmail.com>
2022-05-07 20:02:25 -0300

    django admin

--- aplv/admin.py
+++ aplv/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from aplv.models import Aplv
+
+admin.site.register(Aplv)
+
--- aplvdjango/urls.py
+++ aplvdjango/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin', admin.site.urls),
-]
+
  
 urlpatterns = [ 
+    path('admin/', admin.site.urls),
     path(r'', include('aplv.urls')),
 ]
 
